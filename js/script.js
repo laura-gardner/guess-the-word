55f0e451f4e2902a6b833e6c741b6bf386d9c1fa
laura-gardner <elgardner@gmail.com>
2022-07-25 12:47:06 +0100

    added global variables and functions to replace word with placeholder and to capture value of player guess

---
+++ js/script.js
@@ -0,0 +1,32 @@
+//Creating global variobles
+
+const guessedLetters = document.querySelector(".guessed-letters");
+const guessButton = document.querySelector(".guess");
+const letterInput = document.querySelector(".letter");
+const wordInProgress = document.querySelector(".word-in-progress");
+const remainingGuesses = document.querySelector(".remaining");
+const remainingGuessesDisplay = document.querySelector(".remaining span");
+const message = document.querySelector(".message");
+const playAgainButton = document.querySelector(".play-again");
+const word = "magnolia";
+
+// function to replace letters of word with ● at start of game
+
+const addPlaceholders = function (word) {
+    const placeholder = word.split("");
+    for (let i=0; i<placeholder.length; i++) {
+        placeholder[i] = "●";
+    }
+    const hiddenWord = placeholder.join("");
+    wordInProgress.innerText = hiddenWord;
+}; 
+
+
+// add event listener for click event on guessButton
+
+guessButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    const guess = letterInput.value;
+    console.log(guess);
+    letterInput.value = "";
+});
