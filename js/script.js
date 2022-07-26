4326b9aef3e5b8902e8248f9b35614c9ea0fb286
laura-gardner <elgardner@gmail.com>
2022-07-26 15:28:05 +0100

    Added functions for finishing game and starting new game

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ const remainingGuessesDisplay = document.querySelector(".remaining span");
 const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 let word = "magnolia";
-const guessedLetterArray = [];
+let guessedLetterArray = [];
 let numGuessesRemaining = 8;
 
 const getWord = async function () {
@@ -34,7 +34,7 @@ const addPlaceholders = function (word) {
     wordInProgressDisplay.innerText = hiddenWord;
 }
 
-// add event listener for click event on guessButton
+// event listener for click event on guessButton
 
 guessButton.addEventListener("click", function (e) {
     e.preventDefault();
@@ -98,7 +98,6 @@ const updateWordInProgress = function (guessedLetterArray) {
     }); 
     const wordInProgress = wordArray.join("");
     wordInProgressDisplay.innerText = wordInProgress;
-    console.log(wordInProgress);
     isGameOver(wordInProgress);
 };
 
@@ -109,12 +108,14 @@ const countGuesses = function (guess) {
     if (wordUpper.indexOf(guess) === -1) {
         message.innerText = `The word does not include the letter ${guess}`;
         numGuessesRemaining -= 1;
-        console.log(numGuessesRemaining);
+        remainingGuessesDisplay.innerText = `${numGuessesRemaining} guesses`
     } else {message.innerText = `The word does contain the letter ${guess}!`};
     if (numGuessesRemaining === 0) {
-        message.innerHTML = `You've run out of guesses.  Game over!<br>The word was ${wordUpper}`; 
+        message.innerHTML = `Game over! The word was ${wordUpper}`; 
+        remainingGuesses.innerText = `You've run out of guesses.  Game over!`
+        startOver();
     } else if (numGuessesRemaining === 1) {
-        message.innerText = "You only have one guess remaining!";
+        remainingGuesses.innerText = "You only have one guess remaining!";
     }
 };
 
@@ -125,6 +126,32 @@ const isGameOver = function (wordInProgress) {
     if (wordInProgress === word.toUpperCase()) {
         message.classList.add("win");
         message.innerHTML = '<p class="highlight">You guessed correct the word! Congrats!</p>';
-    } else {console.log("Keep guessing!")}
+        startOver();
+    }
 };
 
+// function to show and hide elements in order to start new game
+
+const startOver = function () {
+    guessButton.classList.add("hide");
+    remainingGuesses.classList.add("hide");
+    guessedLetters.classList.add("hide");
+    playAgainButton.classList.remove("hide");
+};
+
+// event listener for Play Again button
+
+playAgainButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    message.classList.remove("win");
+    message.innerText = "";
+    guessedLetters.innerHTML = "";
+    numGuessesRemaining = 8;
+    guessedLetterArray = [];
+    remainingGuesses.innerText = "You have 8 guesses remaining";
+    playAgainButton.classList.add("hide");
+    guessButton.classList.remove("hide");
+    remainingGuesses.classList.remove("hide");
+    guessedLetters.classList.remove("hide");
+    getWord();
+});
